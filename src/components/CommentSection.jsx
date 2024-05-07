6ac7c75c1031d376f9c8c472ab5c6c43f633efc8
Dominic Francis <dominicf09@gmail.com>
2024-05-07 14:46:04 +0100

    Feature: Add comment section to invidual article pages
    Can now see comments on an article underneath the article

---
+++ src/components/CommentSection.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getCommentsByArticleId } from "../modules/api-requests";
+import CommentCard from "./CommentCard";
+
+export default function CommentSection() {
+  const [comments, setComments] = useState([]);
+  const { article_id } = useParams();
+
+  useEffect(() => {
+    async function fetchComments() {
+      const { comments } = await getCommentsByArticleId(article_id);
+      setComments(comments);
+    }
+    fetchComments();
+  }, []);
+
+  return (
+    <article>
+      <h2>Comments</h2>
+      <ul>
+        {comments.map((comment) => {
+          const date = comment.created_at.split("T")[0];
+          return (
+            <li key={comment.comment_id}>
+              <CommentCard comment={comment} date={date} />
+            </li>
+          );
+        })}
+      </ul>
+    </article>
+  );
+}
